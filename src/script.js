39e2448b8c740ad2daa2becae9ce57bc78d091e6
yunjindev <hunterweisenbach@me.com>
2026-03-03 21:19:53 -0500

    Calculator logic made

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
 const displayText = document.getElementById("display");
+
+function addToDisplay(button) {
+    displayText.value += button;
+}
+
+function clearText() {
+    displayText.value = "";
+}
+
+function calculate() {
+    try {
+        displayText.value = eval(displayText.value)
+    }
+    catch(err) {
+        displayText.value = "Error"
+    }
+}
